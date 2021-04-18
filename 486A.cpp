ebd58c29f5fa745b2363ce3a72f489f12b7d509d
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-18 16:21:17 +0600

    Codeforces Round #277 (Div. 2)

---
+++ 486A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<set>
+#define pi 3.1415
+#define ll long long
+using namespace std;
+int main()
+{   
+   ll int n;
+   cin>>n;
+   ll int sum=(n/2)-n*(n%2);
+   cout<<sum<<endl;
+    return 0;
+}
