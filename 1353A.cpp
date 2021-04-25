6ef3a64f17a49ec3000f7198522c50df8955dd6a
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-25 16:16:58 +0600

    Codeforces Round #642 (Div. 3)

---
+++ 1353A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--){
+int n,m;
+cin>>n>>m;
+if(n==1)
+cout<<0<<endl;
+if(n==2)
+cout<<m<<endl;
+if(n>2)
+cout<<m*2<<endl;
+    
+}
+
+ 
+return 0;
+}
+
