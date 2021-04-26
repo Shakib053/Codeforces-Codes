ba1ec4b20c00ca3af02775a2e0ef9568babd8a52
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-27 00:58:14 +0600

    Codeforces Round #624 (Div. 3)

---
+++ 1311A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{
+   int a,b;
+   cin>>a>>b;
+   if(a==b)
+   cout<<0<<endl;
+   else if(a>b)
+   {
+       if((a-b)%2==0)
+       cout<<1<<endl;
+       else
+       cout<<2<<endl;
+   }
+   else if(a<b)
+   {
+       if((b-a)%2==0)
+       cout<<2<<endl;
+       else
+       cout<<1<<endl;
+   }
+}
+
+
+return 0;
+}
