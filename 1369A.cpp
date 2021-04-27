734a54887863b8c3f89ac35b35dc101d998a3771
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-27 15:37:37 +0600

    Codeforces Round #652 (Div. 2)

---
+++ 1369A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+    unsigned ll int n;
+    cin>>n;
+    if(n%4==0)
+    cout<<"YES\n";
+    else
+    cout<<"NO\n";
+   
+}
+
+
+return 0;
+}
