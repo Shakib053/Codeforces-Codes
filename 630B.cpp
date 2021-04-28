17a63c1f03ea2f6bfea0fab42ab07fc7426ee3f1
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-29 00:43:00 +0600

    Educational Round: VolBIT Formulas Blitz

---
+++ 630B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+double n,t;
+cin>>n>>t;
+double ans=pow(1.000000011,t);
+cout<<n*ans<<endl;
+
+return 0;
+}
