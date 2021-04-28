1126d5002097b921415a31f8ec2fded65f334403
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-29 02:04:34 +0600

    Educational Round: VolBIT Formulas Blitz

---
+++ 630J.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+unsigned ll int n;
+cin>>n;
+cout<<n/2520<<endl;
+
+
+return 0;
+}
