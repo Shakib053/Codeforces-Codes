efe5f3afa94f4e77c3b9d294b4432eeed78eab62
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-29 04:08:00 +0600

    Codeforces Round #611 (Div. 3)

---
+++ 1283A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+    int h,m;
+    cin>>h>>m;
+    int h1=24-h;
+    //int m1=60-m;
+    cout<<(h1*60)-m<<endl;
+    
+   
+}
+
+
+return 0;
+}
