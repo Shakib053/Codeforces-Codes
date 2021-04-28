3029380273c60f2374038ea55339e7a646745033
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-29 01:04:06 +0600

    VolBIT Formulas Blitz

---
+++ 630C.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int mn=n+1;
+unsigned ll int ans=pow(2,mn);
+cout<<ans-2<<endl;
+return 0;
+}
