9bb162bbc8fbab6748e3dca036997158536171ca
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-29 23:04:17 +0600

    Codeforces Round 108 (Rated for Div. 2)

---
+++ 1519B.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include <math.h>
+#include<numeric>
+#define ll long long
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+   int n,m,k;
+  	cin>>n>>m>>k;
+  	if(k==(n*m)-1)
+      cout<<"YES\n";
+      else
+      cout<<"NO\n";
+}
+
+
+return 0;
+}
