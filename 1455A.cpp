129bd8e26be52017881b68753d33144fa50977b6
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-03 01:55:42 +0600

    Round 99 (Rated for Div. 2)

---
+++ 1455A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include <math.h>
+#include<numeric>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+    string s;
+    cin>>s;
+    cout<<s.size()<<endl;
+    
+   
+}
+
+
+return 0;
+}
