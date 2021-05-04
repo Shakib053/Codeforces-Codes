c4cd9863ed8bae6bf94f298ba8ee184567d96137
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-05 03:23:04 +0600

    Codeforces Round #531 (Div. 3)

---
+++ 1102A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include <math.h>
+#include<numeric>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+int main()
+{
+unsigned ll int n;
+cin>>n;
+unsigned ll int z=(n*(n+1))/2;
+if(z%2==0)
+cout<<0<<endl;
+else
+cout<<1<<endl;
+
+return 0;
+}
