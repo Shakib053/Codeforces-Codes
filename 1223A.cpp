c3b6681782f004bbfb149042ad998efb8c775cfd
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-05 15:31:56 +0600

    Technocup 2020

---
+++ 1223A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include <math.h>
+#include<numeric>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+    int n;
+    cin>>n;
+    if(n==2)
+    cout<<2<<endl;
+    else if(n%2==0)
+    cout<<0<<endl;
+    else
+    cout<<1<<endl;
+    
+   
+}
+
+
+return 0;
+}
