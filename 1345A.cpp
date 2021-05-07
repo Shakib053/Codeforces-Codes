1da32fd332fb5523661ec8e984256b74ca2c7141
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-07 19:13:58 +0600

    Codeforces Round #639 (Div. 2)

---
+++ 1345A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+    int n,m;
+    cin>>n>>m;
+    if(n==1 || m==1)
+    cout<<"YES\n";
+    else if(n==2 && m==2)
+    cout<<"YES\n";
+    else
+    cout<<"NO\n";
+    
+   
+}
+
+return 0;
+}
