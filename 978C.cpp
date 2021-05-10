b00d5781f1819aa4b2f51fb91600ea09916192ef
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-10 15:48:23 +0600

    Codeforces Round #481 (Div. 3)

---
+++ 978C.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+un ll n,m;
+cin>>n>>m;
+un ll dm[n];
+un ll ln[m];
+for(int i=0;i<n;i++)
+cin>>dm[i];
+for(int i=0;i<m;i++)
+cin>>ln[i];
+un ll sum=0;
+int j=0;
+for(int i=0;i<m;i++)
+{ 
+ while (sum+dm[j]<ln[i])
+ {
+     sum=sum+dm[j++];
+ }
+ cout<<j+1<<" "<<ln[i]-sum<<endl;
+  
+}
+
+return 0;
+}
