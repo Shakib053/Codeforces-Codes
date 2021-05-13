ee85d2f197c2379f22fbd505e9e869788cf98c5d
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-14 01:09:21 +0600

    Rockethon 2015

---
+++ 513A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int n1,n2,k1,k2;
+cin>>n1>>n2>>k1>>k2;
+if(n1>n2)
+cout<<"First\n";
+else
+cout<<"Second\n";
+
+
+return 0;
+}
