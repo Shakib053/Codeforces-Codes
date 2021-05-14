8404d19060388e41b1d03df7c7b82117a38f9e5d
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-14 22:08:31 +0600

    Codeforces Round #534 (Div. 2)

---
+++ 1104A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+cout<<n<<endl;
+for(int i=0;i<n;i++)
+cout<<1<<" ";
+cout<<endl;
+return 0;
+}
