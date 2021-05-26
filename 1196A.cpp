64420b094ea74264f5a95fe8045a7c2ed75f5c04
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-26 20:14:43 +0600

    Codeforces Round #575 (Div. 3)

---
+++ 1196A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while (t--)
+{ 
+un ll a,b,c;
+cin>>a>>b>>c;
+un ll ans=(a+b+c)/2;
+cout<<ans<<endl;
+}
+
+
+return 0;
+}
