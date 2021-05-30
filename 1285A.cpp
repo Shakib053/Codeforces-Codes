f7f0b127c25cfcb9f873b0d9beedfa0cc31f6e84
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-05-30 12:35:40 +0600

    Codeforces Round #613 (Div. 2)

---
+++ 1285A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#define ll  long long int
+#define un  unsigned
+#define mod 1000000007
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+string s;
+cin>>s;
+cout<<s.size()+1<<endl;
+
+
+
+return 0;
+}
